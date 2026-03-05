4f5e290070efd3723bb0b21114276d6f1a40f006
dkbk <dkbk.git@gmail.com>
2026-03-05 20:30:11 +0900

    ソートをベタ書きからsorted()に変更

--- python/obs80_sort.py
+++ python/obs80_sort.py
@@ -16,32 +16,19 @@ if __name__ == "__main__":
     if not os.path.exists(rfn):
         sys.exit()
 
-    obsList = []
+    pdsList = []
     with open(rfn, "r") as f:
-        obsList = f.readlines()
+        pdsList = f.readlines()
 
-    obsListRange = len(obsList)
+    pdsSortList = []
+    for n in range(len(pdsList)):
+        s = pdsList[n]
+        pdsSortList.append([])
+        pdsSortList[n].append(s)
+        pdsSortList[n].append(s[5:12])
 
-    for n1 in range(obsListRange - 1):
-
-        n1s = obsList[n1]
-        n1n = n1s[0:12]
-
-        swapnum1 = n1
-        swapnum2 = n1
-
-        for n2 in range(n1 + 1, obsListRange):
-
-            n2s = obsList[n2]
-            n2n = n2s[0:12]
-
-            if n2n < n1n:
-                n1n = n2n
-                swapnum2 = n2
-
-        if swapnum1 != swapnum2:
-            obsList[swapnum1], obsList[swapnum2] = obsList[swapnum2], obsList[swapnum1]
+    pdsSortList = sorted(pdsSortList, key=lambda x:x[1])
 
     with open(wfn, "w") as f:
-        for n in range(len(obsList)):
-            f.write(obsList[n])
+        for n in range(len(pdsSortList)):
+            f.write(pdsSortList[n][0])
